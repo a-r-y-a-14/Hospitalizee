b74308b6e5a039de76ecd28fdaa7f06f90788380
Arya <manandipdas@gmail.com>
2026-01-02 03:38:54 +0530

    yee

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date
 from sqlalchemy.dialects.postgresql import JSON
+import math
 
 app = Flask(__name__)
-app.secret_key = "this_is_a_secret_key_change_it"
+app.secret_key = "dont_look_at_my_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospitalizee.db'
 db = SQLAlchemy(app)
+
+today = date.today()
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371  # Earth radius in KM
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat/2)**2 + \
+        math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
+
 
 #db_models
 class Patient(db.Model):
@@ -33,6 +46,7 @@ class Hospital(db.Model):
     lat = db.Column(db.Float, nullable=True)
     lon = db.Column(db.Float, nullable=True)
     emergency_capacity = db.Column(db.Integer, nullable=False)
+    cur_emergency_availability = db.Column(db.Integer, nullable=False, default=emergency_capacity)
 
 class Departments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +56,8 @@ class Doctor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     department = db.Column(db.String(100), db.ForeignKey('departments.id'), nullable=False)
+    qualification = db.Column(db.String(200), nullable=False)
+    experience = db.Column(db.Integer, nullable=False)
     hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
     slots = db.Column(JSON, nullable=False)
 
@@ -91,7 +107,9 @@ def patient_dashboard():
     if 'user_id' not in session:
         return redirect('/patient/login')
     user = Patient.query.get(session['user_id'])
-    return render_template('patient_dashboard.html', user=user)
+    u_app = db.session.query(Appointment, Doctor.name.label('doctor_name'), Hospital.name.label('hospital_name')).join(Doctor, Appointment.doctor_id == Doctor.id).join(Hospital, Appointment.hospital_id == Hospital.id).filter(Appointment.patient_id == user.id, Appointment.appointment_date >= today).all()
+    p_app = db.session.query(Appointment, Doctor.name.label('doctor_name'), Hospital.name.label('hospital_name')).join(Doctor, Appointment.doctor_id == Doctor.id).join(Hospital, Appointment.hospital_id == Hospital.id).filter(Appointment.patient_id == user.id, Appointment.appointment_date < today).all()
+    return render_template('patient_dashboard.html', user=user, u_app=u_app, p_app=p_app)
 
 @app.route('/patient/logout')
 def logout():
@@ -204,6 +222,8 @@ def hospital_register():
         lat = request.form.get('lat')
         lon = request.form.get('long')
 
+        emergency_capacity = request.form.get('emergency_capacity')
+
 
         if Hospital.query.filter_by(email=email).first():
             return render_template(
@@ -212,7 +232,7 @@ def hospital_register():
                 redirect_url="/hospital/login"
             )
 
-        new_user = Hospital(gid=gid, email=email, password=password, name=name, telephone=tel, pincode=pincode, address=address, lat=lat, lon=lon, emergency_capacity=0)
+        new_user = Hospital(gid=gid, email=email, password=password, name=name, telephone=tel, pincode=pincode, address=address, lat=lat, lon=lon, emergency_capacity=emergency_capacity)
         db.session.add(new_user)
         db.session.commit()
 
@@ -227,6 +247,33 @@ def hospital_register():
 @app.route('/emergency')
 def emergency():
     return render_template('emergency.html')
+
+@app.route('/emergency_hosp', methods=['POST'])
+def emergency_hosp():
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    dob = request.form.get('dob')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+    address = request.form.get('address')
+    pincode = request.form.get('pincode')
+    reason = request.form.get('emergency_reason')
+
+    lat = request.form.get('lat')
+    lon = request.form.get('long')
+
+    if lat and lon:
+        all_hospitals = Hospital.query.all()
+        sorted_hospitals = sorted(hospitals, key=lambda h: haversine(float(lat), float(lon), h.lat, h.lon))
+        hospitals = [h for h in sorted_hospitals if h.cur_emergency_availability > 0][:3]
+    else:
+        hospitals = Hospital.query.filter_by(pincode=pincode).limit(3).all()
+
+    return render_template('emergency_rec.html', fname=fname, lname=lname, dob=dob, phone=phone, email=email, address=address, pincode=pincode, reason=reason, hospitals=hospitals)
+
+@app.route('/emergency/book-emergency', methods=['POST'])
+def
+
 
 if __name__ == '__main__':
     app.run(debug=True)
